d818a2d1f0fd907e4fd13407f0152aaf331705c8
Duane M. Moody & Gabriel Puello <student@epicodus.com>
2015-01-28 10:30:01 -0800

    add index and questions routes

--- app.rb
+++ app.rb
@@ -4,5 +4,13 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @question = Question.all()
+  erb(:index)
+end
 
+post('/questions') do
+  question = params.fetch('question')
+  @question = Question.new({:question => question})
+  @task.save()
+  erb(:success)
 end
